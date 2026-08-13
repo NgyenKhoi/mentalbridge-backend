7d1909106e232e57cd3a135890e8d10d34389816
NgyenKhoi <vunguyenkhoi47@gmail.com>
2026-08-13 14:42:39 +0700

    chore(identity): scaffold spring boot service

---
+++ identity-service/src/test/java/com/mentalbridge/identity/IdentityServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.mentalbridge.identity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class IdentityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
